eac7b51bbcb04b7b5dc7f878f1e60898691e7846
Rinat Musin <rinatm@u.yale-nus.edu.sg>
2024-03-01 16:40:17 +0800

    Added functionality to read files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 
-use std::env;
+use std::{env, fs};
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_path = &args[2];
     println!("Query: {}", query);
     println!("Path: {}", file_path);
+
+    let file = fs::read_to_string(file_path).expect("There should be existing file!");
+
+    println!("Contents of file: \n{file}");
     // dbg!(args);
+    //cargo run -- lol src/text.txt
 }
